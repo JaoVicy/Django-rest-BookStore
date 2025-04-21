7f1484e37cd53e0aa334100b2559c790b8b3f97a
JaoVicy <filipimjoa@gmail.com>
2025-04-21 11:48:54 -0300

    All test created

--- product/viewsets/category_viewset.py
+++ product/viewsets/category_viewset.py
@@ -1,2 +1,10 @@
-from product.models import category
+from product.models import Category
 from product.serializers import CategorySerializer
+
+from rest_framework.viewsets import ModelViewSet
+
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self): # queryset is a method
+        return Category.objects.all()
