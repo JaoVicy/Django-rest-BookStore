3ffcbf4b81f806d044495351ce1f8bb6454e5243
JaoVicy <filipimjoa@gmail.com>
2025-07-04 19:28:05 -0300

    Update product_viewset.py imports

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,4 +1,3 @@
-from rest_framework.permissions import IsAuthenticated
 from product.models import Product
 from product.serializers import ProductSerializer
 from rest_framework.viewsets import ModelViewSet
