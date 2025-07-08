9e11d52f60a3a2eeaaf7aefc78fd5205f6437411
JaoVicy <filipimjoa@gmail.com>
2025-07-08 15:02:47 -0300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()  # Isso carrega as variáveis do arquivo .env na raiz do projeto
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
